f57d708a4a44753099f4465c0638b33e1b56e2a6
Allex Lima <allexlima97@gmail.com>
2016-08-05 01:45:09 -0400

    Sessão about finalizada

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 $(document).foundation();
 
 $(document).ready(function(){
-    $("canvas").mouseover(function(){
-        SoundIntro.volume = 1.0;
-        SoundBackground.volume = 1.0;
-    });
-    $("canvas").mouseout(function(){
-        SoundIntro.volume = 0.0;
-        SoundBackground.volume = 0.0;
-    });
+    $("canvas").hover(
+        function() {
+            SoundIntro.volume = 0.6;
+            SoundBackground.volume = 0.6;
+            SoundExplosion.volume = 1.0;
+            SoundUp.volume = 1.0;
+            SoundLose.volume = 1.0;
+        },
+        function() {
+            SoundIntro.volume = 0.0;
+            SoundBackground.volume = 0.0;
+            SoundExplosion.volume = 0.0;
+            SoundUp.volume = 0.0;
+            SoundLose.volume = 0.0;
+        }
+    )
 })
